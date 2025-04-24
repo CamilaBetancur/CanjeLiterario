acdbfb53d0840f09241d628cb2b0cafa9f091eda
camilabema1 <camilabema1@gmail.com>
2025-04-24 19:48:04 +0000

    Conexion con firebase al fin

---
+++ infraestructure/config/firebaseConfig.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import Constants from "expo-constants";
+
+if (!Constants.expoConfig?.extra?.apiKey) {
+  throw new Error("Firebase API Key no está definida en app.config.js");
+}
+const firebaseConfig = {
+  apiKey: Constants.expoConfig?.extra.apiKey,
+  authDomain: Constants.expoConfig?.extra.authDomain,
+  projectId: Constants.expoConfig?.extra.projectId,
+  storageBucket: Constants.expoConfig?.extra.storageBucket,
+  messagingSenderId: Constants.expoConfig?.extra.messagingSenderId,
+  appId: Constants.expoConfig?.extra.appId,
+};
+
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
